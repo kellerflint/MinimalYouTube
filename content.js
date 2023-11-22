099e90d7540442271e184ab53b720ac76d48dd52
Keller Flint <kflint0068@gmail.com>
2023-11-22 10:14:17 -0700

    improving home page video removal

--- content.js
+++ content.js
@@ -1,24 +1,28 @@
 const removeElements = () => {
-    let items = document.getElementsByClassName("ytd-rich-grid-renderer");
+    console.log('ran remove');
+
+    const homeContainer = document.getElementsByTagName(
+        'ytd-two-column-browse-results-renderer'
+    );
+
+    if (homeContainer[0]) {
+        homeContainer[0].remove();
+    }
+
+    let items = document.getElementsByTagName(
+        'ytd-watch-next-secondary-results-renderer'
+    );
 
     for (let i = 0; i < items.length; i++) {
         items[i].remove();
     }
 
-    items = document.getElementsByTagName("ytd-watch-next-secondary-results-renderer");
-
-    for (let i = 0; i < items.length; i++) {
-        items[i].remove();
-    }
-    
-    const shortsContainer = document.getElementById("shorts-inner-container");
+    const shortsContainer = document.getElementById('shorts-inner-container');
     if (shortsContainer) {
-      shortsContainer.remove();
+        shortsContainer.remove();
     }
-}
+};
 
 removeElements();
 
-// TODO: Figurue out how to do this less stupidly. Since the page does everything via ajax have to find a way of running removeElements after the page finishes loading content
 setInterval(removeElements, 200);
-
